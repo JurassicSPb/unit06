e71c1581169e59e2debe6fd703ebf594608d0763
JurassicSPb[credential] <Jurassic5@yandex.ru>
2017-03-22 09:56:54 +0300

    creation test added

--- src/test/java/task02/PropertiesReaderToHashMapTest.java
+++ src/test/java/task02/PropertiesReaderToHashMapTest.java
@@ -3,20 +3,29 @@ package task02;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
+
 /**
  * Created by Мария on 22.03.2017.
  */
 public class PropertiesReaderToHashMapTest {
+    PropertiesReaderToHashMap readerToHashMap;
 
     @Before
     public void init(){
-
+        readerToHashMap = new PropertiesReaderToHashMap();
     }
 
 
     @Test
-    public void testThatWeCanNewHashMap() {
-
+    public void testThatWeCanCreateNewHashMap() {
+        HashMap <String, String> emptyMap = readerToHashMap.getPropertiesToHashMap("", "");
+        assertThat(emptyMap, is(notNullValue()));
+        
     }
 
     @Test
